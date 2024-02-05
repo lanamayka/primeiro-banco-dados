72470840c978f6435db2bf9ba604640ac1842d47
lanamayka <156118677+lanamayka@users.noreply.github.com>
2024-02-05 17:52:28 -0300

    banco de dados em andamento

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,11 @@ import express from 'express';
 import { db, firestore } from '../banco_de_dados/firebase';
 
 const app = express();
-
 app.use(express.json())
 
 app.get('/',(req, res) =>{
     res.send('olá, seja bem vindo a minha primeira API')
-})
-
-app.listen(3000, function(){
-    console.log("servidor rodando na porta http://localhost:3000")
 });
-
 app.post('/usuario', async (req, res) => {
     const nome = req.body.nome;
     const email = req.body.email
@@ -30,6 +24,24 @@ app.post('/usuario', async (req, res) => {
     }catch (e) {
         console.log(e)
         res.status(500).send(e)
-        
     }
 })
+app.get('/listarUsuarios', async (req, res)=>{
+ try {
+    const usuarios = await firestore.getDocs(firestore.collection(db,'usuarios'))
+
+    const usuariosLista = usuarios.docs.map((doc)=>({
+        id: doc.id,
+        ...doc.data(),
+    }))
+    
+    res.send(usuariosLista)
+ } catch (e) {
+    console.log("Erro ao listar usuarios:" + e)
+
+    res.status(500).send("Erro ao listar usuarios" + e)
+ }
+})
+app.listen(3000, function(){
+    console.log("servidor rodando na porta http://localhost:3000")
+});
