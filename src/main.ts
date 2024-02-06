8f5bcdb07f4e9563da98a5ae8c7ea01c7e4bb3ab
lanamayka <156118677+lanamayka@users.noreply.github.com>
2024-02-06 17:21:20 -0300

    atualizei um usuario

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import { db, firestore } from '../banco_de_dados/firebase';
 
 const app = express();
+
 app.use(express.json())
 
 app.get('/',(req, res) =>{
+
     res.send('olá, seja bem vindo a minha primeira API')
+
 });
 app.post('/usuario', async (req, res) => {
+
     const nome = req.body.nome;
     const email = req.body.email
     const telefone = req.body.telefone
@@ -22,7 +26,9 @@ app.post('/usuario', async (req, res) => {
 
     res.send( "usuario adicionado com sucesso " + docRef.id);
     }catch (e) {
+
         console.log(e)
+
         res.status(500).send(e)
     }
 })
@@ -42,6 +48,24 @@ app.get('/listarUsuarios', async (req, res)=>{
     res.status(500).send("Erro ao listar usuarios" + e)
  }
 })
+
+app.put('/atualizarUsuario/:id', async(req, res)=> {
+const id = req.params.id
+const nome = req.body.nome
+
+try {
+    await firestore.updateDoc(firestore.doc(db,'usuarios', id), {
+       nome: nome, 
+    })
+    res.send('Usuario Atualizado com sucesso!')
+} catch (e) {
+    console.log('Erro ao atualizar usuario' + e )
+
+    res.status(500).send('Erro ao atualizar usuario:' + e)
+}
+})
+
+
 app.listen(3000, function(){
     console.log("servidor rodando na porta http://localhost:3000")
 });
